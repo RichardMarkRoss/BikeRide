76dd8b00d442f5d1bc54482440011bfbdaaea408
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-03-19 16:09:06 +0200

    creating test for the fun ride class

--- tests/bicycles/models/testBicycleFromSpec.java
+++ tests/bicycles/models/testBicycleFromSpec.java
@@ -8,18 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class testBicycleFromSpec {
 
     @Test
-    public void shouldTestBikeFromSpecsTandem(){
+    public void shouldTestBikeFromSpecsBrake(){
         BicycleSpecification bikeSpec = new BicycleSpecification(4 ,8 , BicycleType.Tandem);
         BicycleFromSpec bikeFromSpec = new BicycleFromSpec(bikeSpec);
 
-        assertEquals(bikeFromSpec.currentSpeed(), 0);
+        assertEquals(bikeFromSpec.bicycle.getBrakeSpeed(), 8);
     }
 
     @Test
-    public void shouldTestBikeFromSpecsMountBike(){
+    public void shouldTestBikeFromSpecsAccel(){
         BicycleSpecification bikeSpec = new BicycleSpecification(4 ,8 , BicycleType.MountainBike);
         BicycleFromSpec bikeFromSpec = new BicycleFromSpec(bikeSpec);
 
-        assertEquals(bikeFromSpec.bicycle, );
+        assertEquals(bikeFromSpec.bicycle.getAccelerationSpeed(), 4);
+    }
+    @Test
+    public void shouldTestBikeFromSpecsType(){
+        BicycleSpecification bikeSpec = new BicycleSpecification(4 ,8 , BicycleType.Tandem);
+        BicycleFromSpec bikeFromSpec = new BicycleFromSpec(bikeSpec);
+
+        assertEquals(bikeFromSpec.bicycle.getBicycleType(), BicycleType.Tandem);
     }
 }
